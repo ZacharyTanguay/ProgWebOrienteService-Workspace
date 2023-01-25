ba5bbfb5120157a3eaf6ae5c974bf614ab253ca4
Zack <2176651@cegepmontpetit.ca>
2023-01-25 12:32:31 -0500

    constructor

--- lab1/src/app/app.component.spec.ts
+++ lab1/src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'lab1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('lab1');
   });
 
   it('should render title', () => {
--- lab1/src/app/app.component.ts
+++ lab1/src/app/app.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   cheapWisdom = 'Cheap wisdom is expensive';
-  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
   n ?: number;
+
+  constructor() {
+    this.n = 13;
+  }
+
 }
