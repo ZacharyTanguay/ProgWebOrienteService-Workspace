8109b07fdbbff67b2482b825153a8e328f5751f6
Zack <2176651@cegepmontpetit.ca>
2023-02-07 11:03:33 -0500

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,18 +19,19 @@ export class AppComponent {
 
   async searchArtist():Promise<void>{
     this.result = true;
-
     let x = await lastValueFrom(this.http.get<any>("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + this.artist + "&api_key=e34ebf8561ba7c653a21d1d99a1a0070&format=json"));
            
-      for(let a of x.similarartists.artist){
-        this.similarArtists.push(a.name);
+    console.log(x);
+    this.artist = x.artist;
+      for(let artist of x.similarartists.artist){
+        this.similarArtists.push(artist.name);
       }
     
    
-    console.log(x);
   }
 
   newSearch():void{
+    this.similarArtists = [];
     this.result = false;
   }
 }
