df83dc3eff46b0dcc1ac30845d2cc61277f4f364
Zack <2176651@cegepmontpetit.ca>
2023-02-14 11:54:35 -0500

    Ajout des components
    Album, artist, show, song

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ArtistComponent } from './artist/artist.component';
+import { AlbumComponent } from './album/album.component';
+import { ShowComponent } from './show/show.component';
+import { SongComponent } from './song/song.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistComponent,
+    AlbumComponent,
+    ShowComponent,
+    SongComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    RouterModule.forRoot([
+      { path: '', component: ArtistComponent },
+      { path: 'artist', component: ArtistComponent },
+      { path: 'album', component: AlbumComponent },
+      { path: 'show', component: ShowComponent },
+      { path: 'song', component: SongComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
